288d10d79cbfa5a92d12fa5dc71e8a117036886d
alamin khan <alaminuixd@gmail.com>
2025-09-09 01:07:02 +0600

    initial commit 2

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
+// third-party modules
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-import fs from "fs/promises";
 import cors from "cors";
 import { v4 as uuidv4 } from "uuid";
 import multer from "multer";
+// core modules
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs/promises";
 
 dotenv.config();
 const app = express();

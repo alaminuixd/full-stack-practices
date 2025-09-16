8103d1b71bfb204a1b0dc438ddfa29852f692b0d
alamin khan <alaminuixd@gmail.com>
2025-09-16 17:23:31 +0600

    initial commit 5

--- backend/bin/expriments.js
+++ backend/bin/expriments.js
@@ -1,11 +1,21 @@
 const file = {
   originalname: "hayman.jpg",
+  mimetype: "image/jpg",
 };
+
 const path = {
   extname(str) {
     const match = str.match(/\.[^.]+$/);
     return match ? match[0] : "";
   },
 };
-const ext = path.extname(file.originalname);
+
+const fileTypes = ["jpg", "jpeg", "png", "gif", "webp"];
+const ext = path.extname(file.originalname).toLowerCase().substring(1);
+const mimeType = file.mimetype.split("/")[1];
+
+if (fileTypes.includes(ext) && fileTypes.includes(mimeType)) {
+  console.log("success");
+}
+
 console.log(ext);

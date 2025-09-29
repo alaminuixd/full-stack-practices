060280f366ac917383b40b5586df55a91494a12c
alaminuixd <alaminuixd@gmail.com>
2025-09-30 01:08:18 +0600

    actor branch initial commit

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3005;
 
-app.use(express.json());
-
 // create necessary directories
 const UPLOAD_DIR = path.join(__root, "public", "uploads");
 const DATA_DIR = path.join(__root, "public", "data");
@@ -21,11 +19,78 @@ const DATA_DIR = path.join(__root, "public", "data");
 const ACTOR_FILES = path.join(UPLOAD_DIR, "singers");
 await fs.mkdir(ACTOR_FILES, { recursive: true });
 await fs.mkdir(DATA_DIR, { recursive: true });
+// data file
+const ACTOR_DATA_PATH = path.join(DATA_DIR, "actors.json");
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/actors", express.static(ACTOR_FILES));
 
 // MULTER starts
+const maxFileSizeMB = 5;
+const allowedFileTypes = ["jpg", "png", "jpeg", "gif", "webp"];
 const storage = multer.diskStorage({
-  destination: (req) => {},
+  destination: async (req, file, cb) => {
+    await fs.mkdir(ACTOR_FILES, { recursive: true });
+    cb(null, ACTOR_FILES);
+  },
+  filename: (req, file, cb) => {
+    const extName = path.extname(file.originalname).toLowerCase();
+    const dateName = new Date().toISOString().split("T")[0];
+    const idName = uuidv4().split("-").pop();
+    const fullName = `${dateName}-${idName}${extName}`;
+    cb(null, fullName);
+  },
+});
+const fileTypesRegx = new RegExp(`\\.${allowedFileTypes.join("|")}`, "i");
+const upload = multer({
+  storage,
+  limits: { fileSize: maxFileSizeMB * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (
+      !fileTypesRegx.test(path.extname(file.originalname).toLowerCase()) ||
+      !allowedFileTypes.includes(file.mimetype.split("/")[1])
+    ) {
+      cb(new Error(`Only ${allowedFileTypes.split(", ")} are allowed.`));
+    }
+    cb(null, true);
+  },
 });
 // MULTER ends
+
+app.post(
+  "/api/actors",
+  upload.fields([{ name: "avatar", maxCount: 1 }]),
+  async (req, res) => {
+    const { name, spouse, imgURL } = req.body;
+    try {
+      let actors = [];
+      try {
+        const data = await fs.readFile(ACTOR_DATA_PATH, "utf8");
+        const parsed = data?.trim() ? JSON.parse(data) : [];
+        actors = Array.isArray(parsed) ? parsed : [];
+      } catch (error) {
+        if (error.code === "ENOENT") {
+          await fs.writeFile(ACTOR_DATA_PATH, "[]");
+          actors = [];
+        } else {
+          throw error;
+        }
+      }
+      const actor = {
+        id: uuidv4(),
+        name,
+        spouse,
+        imgURL: req.files.avatar[0].filename,
+      };
+      actors.push(actor);
+      await fs.writeFile(ACTOR_DATA_PATH, JSON.stringify(actors, null, 2));
+      res.status(201).json({ message: "New actor created", actor });
+    } catch (error) {
+      res.status(500).json({ message: "Server error!" });
+    }
+  }
+);
 
 app.listen(PORT, () => console.log(`Listening to the PORT: ${PORT}`));
